700e67e86ba56ee7371c4e0307a1dfe29a9eeddf
darylldawn <daryllvillaos@gmail.com>
2017-07-06 09:57:53 +0900

    Update CDVDecimalKeyboard.m

--- src/ios/CDVDecimalKeyboard.m
+++ src/ios/CDVDecimalKeyboard.m
@@ -8,6 +8,10 @@ UIView* ui;
 CGRect cgButton;
 BOOL isDecimalKeyRequired=YES;
 UIButton *decimalButton;
+
+BOOL isMinusKeyRequired=YES;
+UIButton *minusButton;
+
 BOOL isAppInBackground=NO;
 - (void)pluginInitialize {
     wv = self.webView;
@@ -51,6 +55,12 @@ BOOL isAppInBackground=NO;
     NSString* decimalChar = [wv stringByEvaluatingJavaScriptFromString:@"DecimalKeyboard.getDecimalChar();"];
     [decimalButton setTitle:decimalChar forState:UIControlStateNormal];
 }
+
+-(void) setMinusChar{
+    NSString* minusChar = "-"
+    [minusButton setTitle:minusChar forState:UIControlStateNormal];
+}
+
 - (void) addDecimalButton{
     if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad )
     {
@@ -100,17 +110,83 @@ BOOL isAppInBackground=NO;
         [ui addSubview:decimalButton];
     }
 }
+
+- (void) addMinusButton{
+    if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad )
+    {
+        return ; /* Device is iPad and this code works only in iPhone*/
+    }
+    minusButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self setMinusChar];
+    [minusButton setTitleColor:[UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:1.0] forState:UIControlStateNormal];
+    minusButton.titleLabel.font = [UIFont systemFontOfSize:40.0];
+    [minusButton addTarget:self action:@selector(buttonPressedm:)
+            forControlEvents:UIControlEventTouchUpInside];
+    [minusButton addTarget:self action:@selector(buttonTappedm:)
+            forControlEvents:UIControlEventTouchDown];
+    [minusButton addTarget:self action:@selector(buttonPressCancelm:)
+            forControlEvents:UIControlEventTouchUpOutside];
+    
+    
+    minusButton.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    [minusButton setTitleEdgeInsets:UIEdgeInsetsMake(-20.0f, 0.0f, 0.0f, 0.0f)];
+    [minusButton setBackgroundColor: [UIColor colorWithRed:210/255.0 green:213/255.0 blue:218/255.0 alpha:1.0]];
+    
+    // locate keyboard view
+    UIWindow* tempWindow = nil;
+    NSArray* openWindows = [[UIApplication sharedApplication] windows];
+    
+    for(UIWindow* object in openWindows){
+        if([[object description] hasPrefix:@"<UIRemoteKeyboardWindow"] == YES){
+            tempWindow = object;
+        }
+    }
+    
+    if(tempWindow ==nil){
+        //for ios 8
+        for(UIWindow* object in openWindows){
+            if([[object description] hasPrefix:@"<UITextEffectsWindow"] == YES){
+                tempWindow = object;
+            }
+        }
+    }
+
+    
+    UIView* keyboard;
+    for(int i=0; i<[tempWindow.subviews count]; i++) {
+        keyboard = [tempWindow.subviews objectAtIndex:i];
+        [self listSubviewsOfView: keyboard];
+        minusButton.frame = cgButton;
+        [ui addSubview:minusButton];
+    }
+}
+
 - (void) removeDecimalButton{
     [decimalButton removeFromSuperview];
     decimalButton=nil;
     stopSearching=NO;
     
 }
+
+- (void) removeMinusButton{
+    [minusButton removeFromSuperview];
+    minusButton=nil;
+    stopSearching=NO;
+    
+}
+
 - (void) deleteDecimalButton{
     [decimalButton removeFromSuperview];
     decimalButton=nil;
     stopSearching=NO;
 }
+
+- (void) deleteMinusButton{
+    [minusButton removeFromSuperview];
+    minusButton=nil;
+    stopSearching=NO;
+}
+
 BOOL isDifferentKeyboardShown=NO;
 
 - (void) keyboardWillAppear: (NSNotification*) n{
@@ -146,6 +222,20 @@ BOOL isDifferentKeyboardShown=NO;
             [self removeDecimalButton];
         }
     }
+
+        // create custom button
+    if(minusButton == nil){
+        if(isMinusKeyRequired){
+            [self addMinusButton];
+        }
+    }else{
+        if(isMinusKeyRequired){
+            minusButton.hidden=NO;
+            [self setMinusChar];
+        }else{
+            [self removeMinusButton];
+        }
+    }
 }
 
 - (void)buttonPressed:(UIButton *)button {
@@ -153,12 +243,27 @@ BOOL isDifferentKeyboardShown=NO;
     [wv stringByEvaluatingJavaScriptFromString:@"DecimalKeyboard.addDecimal();"];
 }
 
+- (void)buttonPressedm:(UIButton *)button {
+    [minusButton setBackgroundColor: [UIColor colorWithRed:210/255.0 green:213/255.0 blue:218/255.0 alpha:1.0]];
+    [wv stringByEvaluatingJavaScriptFromString:@"DecimalKeyboard.addMinus();"];
+}
+
 - (void)buttonTapped:(UIButton *)button {
     [decimalButton setBackgroundColor: [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0]];
 }
+
+- (void)buttonTappedm:(UIButton *)button {
+    [minusButton setBackgroundColor: [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0]];
+}
+
 - (void)buttonPressCancel:(UIButton *)button{
     [decimalButton setBackgroundColor: [UIColor colorWithRed:210/255.0 green:213/255.0 blue:218/255.0 alpha:1.0]];
 }
+
+- (void)buttonPressCancelm:(UIButton *)button{
+    [minusButton setBackgroundColor: [UIColor colorWithRed:210/255.0 green:213/255.0 blue:218/255.0 alpha:1.0]];
+}
+
 -(BOOL)isTextAndDecimal{
     BOOL bln = YES;
     NSString *isText = [wv stringByEvaluatingJavaScriptFromString:@"DecimalKeyboard.getActiveElementType();"];
@@ -213,4 +318,3 @@ BOOL stopSearching=NO;
 
 
 @end
-
